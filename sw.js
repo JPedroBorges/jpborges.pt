da94fd46b38d9cdc0698860031935e3efca4fc07
João Borges <joao.pedro.borges@hotmail.com>
2020-08-16 02:38:02 +0100

    Add service worker for offline

---
+++ sw.js
@@ -0,0 +1,41 @@
+importScripts('/js/asset/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('jpborges').then(function(cache) {
+     return cache.addAll([
+        '/'
+        ,'/index.html'
+        ,'/css/asset/bootstrap.min.css'
+        ,'/css/asset/fontawesome.min.css'
+        ,'/css/asset/w3.css'
+        ,'/css/style.css'
+        ,'/js/asset/jquery.min.js'
+        ,'/js/asset/bootstrap.min.js'
+        ,'/js/script.js'
+        ,'/img/me.webp'
+        ,'/img/work/asseco.webp'
+        ,'/img/work/odkas.webp'
+        ,'/img/work/miti.webp'
+        ,'/img/work/exictos.webp'
+        ,'/img/university/uma.webp'
+        ,'/img/university/um.webp'
+        ,'/img/balcony.webp'
+        ,'/css/webfonts/fa-solid-900.woff2'
+        ,'/css/webfonts/fa-solid-900.woff'
+        ,'/css/webfonts/fa-solid-900.ttf'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
